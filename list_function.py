597f7d7cd75c442bab4d2e6ed652efc3ade95dac
henry-the-vietnamese <henryfromvietnam@gmail.com>
2021-10-14 01:56:25 +1030

    Initial commit - only the description

---
+++ list_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#
+# File:         list_function.py
+# Author:       Tan Duc Mai
+# Email:        tan.duc.work@gmail.com
+# Date:         13/10/2021
+# Description:  Define some of the list functions.
+#   I hereby declare that I completed this work without any improper help
+#   from a third party and without using any aids other than those cited.
+#
