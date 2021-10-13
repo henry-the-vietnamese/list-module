98fd8619cdf1493cb861134584fcd4515b40e444
henry-the-vietnamese <henryfromvietnam@gmail.com>
2021-10-14 02:32:32 +1030

    size

--- list_function.py
+++ list_function.py
@@ -9,3 +9,18 @@
 #   I hereby declare that I completed this work without any improper help
 #   from a third party and without using any aids other than those cited.
 #
+
+
+
+def size(my_list):
+    # Function size()
+    LEN = 0
+    for _ in my_list:
+        LEN += 1
+    return LEN
+
+
+
+
+
+
