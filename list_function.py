bab8b04ef6482e6fc3e8b02e3ffd3e3d1fd182ab
henry-the-vietnamese <henryfromvietnam@gmail.com>
2021-10-15 01:40:20 +1030

    function docstring and variable documentation

--- list_function.py
+++ list_function.py
@@ -13,8 +13,14 @@
 
 
 def size(my_list):
-    # Function size()
-    LEN = 0
+    """ Function size()
+
+    :param my_list: List whose elements are to be counted.
+    :param type: list
+    :returns: The number of elements contained in the list.
+    "rtype: int
+    """
+    LEN = 0         # Initialise a variable to keep track of each element encountered in the list.
     for _ in my_list:
         LEN += 1
     return LEN
@@ -22,12 +28,19 @@ def size(my_list):
 
 
 def to_string(my_list, sep=', '):
-    # Function to_string()
-    if size(my_list) == 0:
+    """ Function to_string()
+
+    :param my_list: List to be converted.
+    :type my_list: list
+    :returns: Result of the conversion of the list.
+    :rtype: str
+    """
+
+    if size(my_list) == 0:      # If the list contains no element (an empty list), returns an empty string.
         return ''
     else:
-        string = ''
-        COUNT = 1
+        string = ''         # Initialise an empty string to be appended later.
+        COUNT = 1           # Initialise a variable to keep track of the location of the current element being iterated.
         for i in my_list:
             if size(my_list) != COUNT:
                 string += (str(i) + sep)
